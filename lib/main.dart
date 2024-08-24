175f51564242961deed5b1bc15d94066f594d855
Shalu sharma <149373622+Shalu6634@users.noreply.github.com>
2024-08-24 09:35:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,3 +24,99 @@ class myApp extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+//class Employees{
+//    int id;
+//    String name;
+//    float salary;
+//    void insertRecord(int i,String n,float s)
+//    {
+//        id = i;
+//        name = n;
+//        salary = s;
+//    }
+//    void displayInfo()
+//    {
+//        out.println(id + " " + name+" "+salary);
+//
+//
+//    }
+//
+//}
+//public class EmployeeTest{
+//    public static void main(String args[]){
+//        String value="3456";
+//        out.println(value.charAt(1));
+//      //printing members with a white space
+//    }
+//}
+
+class Account {
+    int acc_no;
+    String name;
+    float amount;
+
+    void setMethod(int a, String n, float am) {
+        acc_no = a;
+        name = n;
+        amount = am;
+    }
+
+    void deposit(float amt) {
+        amount = amount + amt;
+        out.println(amt + "deposited");
+    }
+
+    void withdraw(float amt) {
+
+        if (amount < amt) {
+            out.println("Insufficient Balance");
+        } else {
+            amount = amount - amt;
+            out.println(amt + "withDrawn");
+        }
+    }
+
+    void checkBalance() {
+        out.println("Balance is " + amount);
+    }
+
+    void display() {
+        out.println("your account details here :-" + acc_no + " " + name + " " + amount);
+    }
+}
+
+
+ class AccountTest {
+    public static void main(String args[]) {
+       Account a1 = new Account();
+       a1.setMethod(1234,"Shalu",10000);
+
+       //display all detail
+       a1.display();
+        //balance show
+       a1.checkBalance();
+       //save balance in bank
+       a1.deposit(3000);
+       //balance check
+       a1.checkBalance();
+       a1.withdraw(200);
+        a1.checkBalance();
+    }
+}
+
